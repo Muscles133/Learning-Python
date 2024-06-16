0db175780087985fcb5c5c80083b010825c38927
Muscles123 <darcytemple1@gmail.com>
2024-06-17 09:53:55 +1000

    week 0 - convert to lowercase done

---
+++ indoor.py
@@ -0,0 +1,4 @@
+yell = input("I need you to yell out anything and then i will whiper it to a friend (capes is preffered) ")
+lowc = yell.lower()
+print("Hey friend i need to whisper this to you. '" + lowc + "'")
+
