f0c73b7701f30da7c8f16cbe50bb24622b2cb0d3
Muscles123 <darcytemple1@gmail.com>
2024-06-18 22:35:50 +1000

    fixed spelling ect

--- deep.py
+++ deep.py
@@ -1,14 +1,7 @@
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-match answer.lower():
-    case "42" | "fourty two" | "fourty-two":
+match answer.replace(" ","").lower():
+    case "42" | "fortytwo" | "forty-two":
         print("Yes")
     case _:
         print("No")
-
-
-
-
-
-    
-    
