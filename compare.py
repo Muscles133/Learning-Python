4b726a7665e6a41cfa6b7ce3ab4302f9b62af946
Muscles123 <darcytemple1@gmail.com>
2024-06-18 15:27:20 +1000

    Week 1 stuff

---
+++ compare.py
@@ -0,0 +1,23 @@
+# x = int(input("whats the x?"))
+# y = int(input("whats the y?"))
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x equals y")
+
+
+x = int(input("whats the x?"))
+y = int(input("whats the y?"))
+
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print('x is equal to y')
+
+if x!= y:
+    print("x is not equel to y")
+else:
+    print("x =y")
---
+++ deep.py
@@ -0,0 +1,15 @@
+Greet = input("Greeting: ")
+
+if Greet.startswith("Hello"):
+    print("$0")
+elif Greet.startswith("H" or "h"):
+    print("$20")
+else:
+    print("$100")
+
+
+
+
+
+    
+    
