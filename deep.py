97de23680af26eecd9462fd7f1ec0ca761cb9f5a
Muscles123 <darcytemple1@gmail.com>
2024-06-18 22:22:03 +1000

    made it case insensitive

--- deep.py
+++ deep.py
@@ -1,7 +1,7 @@
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-match answer:
-    case "42" | "Fourty Two" | "Fourty-Two":
+match answer.lower():
+    case "42" | "fourty two" | "fourty-two":
         print("Yes")
     case _:
         print("No")
