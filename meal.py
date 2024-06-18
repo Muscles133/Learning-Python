c8129b97bbcb03c66d096deff93a7d9338380eb3
Muscles123 <darcytemple1@gmail.com>
2024-06-18 22:03:12 +1000

    week 1 finished challenge mode

--- meal.py
+++ meal.py
@@ -1,23 +1,30 @@
 def main():
     time = convert(input("What time is it? "))
 
-    if  700 >= time < 800:
+    if  7 <= time < 8:
         print("Breakfast time")
-    elif  1200 >= time < 1300:
+    elif  12 <= time < 13:
         print("Lunch time")
-    elif  1800 >= time < 1900:
+    elif  18 <= time < 19:
         print("Dinner time")
-
-
-
-def convert(t):
-    if t.endwith("p.m"):
-        h,m,x = t.split((":", " "))
-        return h + m
     else:
-        h,m = t.split(":")
-        return h + m    
+        print("Unsure what time you are using")
 
+def convert(time):
+    
+    if time.endswith("p.m."):
+        hourmin, ampm = time.split()
+        h, m = hourmin.split(":")
+        return int(h) + int(m)/60 + 12
+    
+    elif time.endswith("a.m."):
+        hourmin, ampm = time.split()
+        h, m = hourmin.split(":")
+        return int(h) + int(m)/60
+    
+    else: 
+        h,m = time.split(":")
+        return int(h) + int(m)/60
 
 if __name__ == "__main__":
     main()
