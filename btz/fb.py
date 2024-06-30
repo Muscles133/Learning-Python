f2059ca73f986e366986f8d209257d3b8d615ad5
Muscles123 <darcytemple1@gmail.com>
2024-06-30 12:54:29 +1000

    start of bot projects

---
+++ btz/fb.py
@@ -0,0 +1,24 @@
+"""
+This is my fishing bot 
+
+"""
+
+# checks for bait
+# apply bait if missing
+# dont look for bait if we dont have anything
+
+# cast fishing pole
+#confirm that the bob is in the correct position
+# if not cast again
+
+#wait for the splash *maybe listen to the splash?
+# catch the fish
+# timer on cast if nothing after X time recast
+# check if bags are full or not
+
+#log out if bags are full
+
+# create a log that counts successfull catches
+# log recasts and non catches
+
+# hot key to start and stop
---
+++ btz/rotation.py
@@ -0,0 +1,50 @@
+"""
+this is my rotation bot
+
+Keep Faerie Fire (Feral) active on the boss.
+Keep Savage Roar up.
+Keep the debuff from Mangle up.
+Cast Tiger's Fury whenever it's available and you have 40 or less energy. ...
+Cast Rip with 5 combo points, if you don't need to recast Savage Roar.
+Cast Shred.
+
+Maintain  Faerie Fire (Feral) Debuff on boss, if no other Druid is applying it.
+Maintain  Savage Roar to increase all damage by 30%.
+Use  Mangle to put the debuff up on the target.
+Use  Shred as your main damage dealer, and filler ability.
+At 5 Combo points, use  Rip as your secondary finisher, and make sure not to clip it.
+Powershift in and out of Catform whenever you are more than 1 energy tick away from another ability with the macro /use !cat form.
+ Mangle again only if it will allow you to Powershift sooner and you will not be waiting for a  Shred
+
+
+"""
+import pyautogui
+import sys
+import time
+import numpy as np
+import keyboard
+
+
+
+def main():
+    #toggle on and toggle off
+    #while on:
+    #if button stop
+
+
+def Burst():
+
+
+def Faerie():
+
+def Savage():
+
+def Mangle():
+
+def tiger():
+#6 sec buf 15% DMG
+# 
+def rip():
+    12 sec duratiuon ss
+
+def Shred():
