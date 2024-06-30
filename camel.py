fb3cf51d8a0ea64e6b025760947a7f9ea5f5f33e
Muscles123 <darcytemple1@gmail.com>
2024-06-30 15:57:57 +1000

    changed

--- camel.py
+++ camel.py
@@ -2,16 +2,12 @@ def main():
     camelcase = "ThisIsACamelString" #input("Convert camelCase to snake_case: ")
     Snake_Convert(camelcase)
 
-
 def Snake_Convert(camelcase):
     for letters in camelcase:
             if letters.islower():
                  print(letters, end='')
             else:
                  letters.lower()
-                 print(f"_ {letters}", end='')
+                 print(f"_{letters.lower()}", end='')
             
-            
-
-
 main()
--- yo.py
+++ yo.py
@@ -1,4 +1,36 @@
-#yoyoyoy
-yo = ("eat a sandwich")
+# #yoyoyoy
+# yo = ("eat a sandwich")
 
-print (yo)
+# print (yo)
+
+def main():                 #dictionary of fruits with calories
+        fruit_calories = {
+            "apple": 130,
+            "avocado": 50,
+            "banana": 110,
+            "cantaloupe": 50,
+            "grapefruit": 60,
+            "grapes": 90,
+            "honeydew melon": 50,
+            "kiwifruit": 90,
+            "lemon": 15,
+            "lime": 20,
+            "nectarine": 60,
+            "orange": 80,
+            "peach": 60,
+            "pear": 100,
+            "pineapple": 50,
+            "plums": 70,
+            "strawberries": 50,
+            "sweet cherries": 100,
+            "tangerine": 50,
+            "watermelon": 80
+        }
+        fruit = input("Item: ").lower() #prompts for the fruit
+        if fruit in fruit_calories:
+              print("Calories: " + str(fruit_calories[fruit])) #outputs the calories
+        else:
+              print()
+
+
+main()
