0f36d4680c5c8bb46ec3ddbd0de35e9f1fb45e11
Muscles123 <darcytemple1@gmail.com>
2024-09-23 16:23:57 +1000

    changes

--- watch.py
+++ watch.py
@@ -1,22 +1,13 @@
 import re
-import sys
-
 
 def main():
-
-    html = r'<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0?si=Bq_NljtVZera5Mk2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
-    #print(html)
-    #print(parse(input("HTML: ")))
-
-    print(parse(html))
-
+    #html = r'<iframe width="560" height="315" src="https://www.youtube.com/embed/DyoVVSggPjY?si=lKDyUT7Qpi24u4KS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    print(parse(input("HTML: ")))
 
 def parse(s):
-    if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", s):
-
-        print(f"Username : ", matches.group(1))
-
-
+    if matches := re.search(r'src="https?://(?:www\.)?youtube\.com/embed/([^"?]+)', s, re.IGNORECASE):
+        return f"https://youtu.be/{matches.group(1)}"
+    return None
 
 if __name__ == "__main__":
     main()
